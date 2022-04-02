ad8c3949224ee7b9ba52b2d83d64b223a6b867ad
Honorata.Lyczak <Honorata.Lyczak@PK5-07>
2022-04-02 15:32:11 +0200

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Interface1.cs
@@ -0,0 +1,7 @@
+﻿namespace ConsoleApp.Logger
+{
+    public interface ILogger : IDisposable
+    {
+        void Log(params String[] messages);
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = "some-domain.com";
+            int port = 80;
+
+            using (ClientSocket clientSocket = new ClientSocket(host, port))
+            {
+                // request:
+
+                string requestText = "Message to sent ...";
+                byte[] requestBytes = Encoding.UTF8.GetBytes(requestText);
+
+                clientSocket.Send(requestBytes);
+
+                // response:
+
+                byte[] responseBuffer = new byte[1024];
+                int responseSize = clientSocket.Receive(responseBuffer);
+
+                string responseText = Encoding.UTF8.GetString(responseBuffer, 0, responseSize); // received message
+
+                // ...
+
+                // cleaning:
+
+                clientSocket.Close();
+            }
+        }
+    }
+}
+
+
